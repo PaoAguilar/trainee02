4b7331f72d8f4dac31d9a84b1a665d80055800e9
Paola Aguilar <pao.aquev29@gmail.com>
2021-03-05 12:15:25 -0600

    adding next five days weather

--- js/dateBuilder.js
+++ js/dateBuilder.js
@@ -1,31 +1,43 @@
-export const dateBuilder = (fullDate) => {
-  var months = new Array(
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  );
-  var days = new Array(
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  );
+const fullDate = new Date();
+var months = new Array(
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+);
+var days = new Array(
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+);
+
+export const currentDate = () => {
   const day = days[fullDate.getDay()];
   const date = fullDate.getDate();
   const month = months[fullDate.getMonth()];
   const year = fullDate.getFullYear();
+  console.log(fullDate);
+  console.log(fullDate.getMonth() + 1);
+  return `${day} ${date} ${month} ${year}`;
+};
 
+export const nextDay = () => {
+  const day = days[fullDate.getDay() + 1];
+  const date = fullDate.getDate() + 1;
+  const month = months[fullDate.getMonth() + 1];
+  const year = fullDate.getFullYear();
+  console.log(fullDate);
   return `${day} ${date} ${month} ${year}`;
 };
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { dateBuilder } from "./dateBuilder.js";
+import { currentDate, nextDay } from "./dateBuilder.js";
 const url =
   "https://secret-ocean-49799.herokuapp.com/https://www.metaweather.com/api/";
 const city = document.querySelector(".location-city");
@@ -6,28 +6,57 @@ const temp = document.querySelector(".temp");
 const weather = document.querySelector(".weather");
 const minMax = document.querySelector(".min-max");
 const locationDate = document.querySelector(".location-date");
-const date = new Date();
+const carrouselItem = document.querySelector(".carousel__container");
 
-const getInitialData = async () => {
+const getDefaultData = async () => {
   try {
     const res = await fetch(`${url}location/2487956/`);
+    const weatherData = await res.json();
 
-    const data = await res.json();
-
-    city.innerText = `${data.title}, ${data.parent.title}`;
+    // data por defecto
+    city.innerText = `${weatherData.title}, ${weatherData.parent.title}`;
     temp.innerHTML = `${Math.round(
-      data.consolidated_weather[0].the_temp
+      weatherData.consolidated_weather[0].the_temp
     )}<span>°c</span>`;
-    weather.innerText = `${data.consolidated_weather[0].weather_state_name}`;
+    weather.innerText = `${weatherData.consolidated_weather[0].weather_state_name}`;
     minMax.innerText = `min ${Math.round(
-      data.consolidated_weather[0].min_temp
-    )}°c / max ${Math.round(data.consolidated_weather[0].max_temp)}°c`;
-    locationDate.innerText = dateBuilder(date);
+      weatherData.consolidated_weather[0].min_temp
+    )}°c / max ${Math.round(weatherData.consolidated_weather[0].max_temp)}°c`;
+    locationDate.innerText = currentDate();
 
-    console.log(data);
+    console.log(weatherData);
+    // data del next day
+    let weatherDataFirst = weatherData.consolidated_weather.slice(1);
+    console.log(weatherDataFirst);
+    weatherDataFirst.map((result) => {
+      console.log(result);
+      carrouselItem.innerHTML += `
+              <div class="carousel-item">
+              <img
+                class="carousel-item__img"
+                src="./img/carrousel.jpg"
+                alt=""
+              />
+                <div class="carousel-item__details">
+                  <div>
+                    <div class="carrousel-temp">${Math.round(
+                      result.the_temp
+                    )}<span>°c</span></div>
+                    <div class="carrousel-weather">${
+                      result.weather_state_name
+                    }</div>
+                    <div class="carrousel-min-max">${Math.round(
+                      result.min_temp
+                    )}°c / ${Math.round(result.max_temp)}°c</div>
+                    <p class="carousel-title">${nextDay()}</p>
+                  </div>
+                </div>
+              </div>
+            `;
+    });
   } catch (error) {
     console.log(error);
   }
 };
 
-getInitialData();
+getDefaultData();
